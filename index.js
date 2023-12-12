d4f0e8fd5c35bc9ecf36645ed2f347a3dc1ca95f
Margulis162 <markmargulis@proton.me>
2023-12-12 17:00:15 -0500

    ejs installed

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ app.get("/", (req, res) => {
     res.send("welcome to express.js server");
 })
 
+
 app.get("/about", (req, res) => {
     res.send("<h1>this is ABOUT tub, !not about TUB");
 })
+
+// to serve static files
+
+app.use(express.static("public"));
 
 // server
 app.listen(3000, () => {
     console.log("Server is up. What's up server?!")
 })
+
+// ABSTRACTCOMMENTS
+// npm i nodemon -g  for global instal of nodemon which does the sercer restart for you automatically when files are updated
+// npm install ejs - to install a templating engine ejs
